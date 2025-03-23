df7fb8585e2c563fe4cabebbfbf331b888758b11
Nishi Surti <01nishisurti@gmail.com>
2025-03-23 14:16:16 -0400

    isGif checked or not

--- Essential-JS-Concepts/Pumpkins-Purfect-Meme-Picker/index.js
+++ Essential-JS-Concepts/Pumpkins-Purfect-Meme-Picker/index.js
@@ -1,22 +1,35 @@
 import { catsData } from "./data.js";
 
 const emotionRadios = document.getElementById("emotion-radios");
+const getImageBtn = document.getElementById("get-image-btn");
+const gifsOnlyOption = document.getElementById("gifs-only-option");
 
 emotionRadios.addEventListener("change", highlightCheckedOption);
 
+getImageBtn.addEventListener("click", getMatchingCatsArray);
+
 function highlightCheckedOption(e) {
   const radios = document.getElementsByClassName("radio");
-
   for (let radio of radios) {
     radio.classList.remove("highlight");
   }
+  // remove all instances of the highlight class
   document.getElementById(e.target.id).parentElement.classList.add("highlight");
-  //change is an event on radio button
+}
+
+function getMatchingCatsArray() {
+  const isGif = gifsOnlyOption.checked;
+  console.log(isGif);
+  if (document.querySelector('input[type="radio"]:checked')) {
+    const selectedEmotion = document.querySelector(
+      'input[type="radio"]:checked'
+    ).value;
+    console.log(selectedEmotion);
+  }
 }
 
 function getEmotionsArray(cats) {
   const emotionsArray = [];
-
   for (let cat of cats) {
     for (let emotion of cat.emotionTags) {
       if (!emotionsArray.includes(emotion)) {
